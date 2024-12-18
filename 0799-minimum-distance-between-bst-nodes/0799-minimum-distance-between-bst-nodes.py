67dda5c0c9bee60c8accab83e6fdb39f081f816d
Anushk Pandey <98946689+pxndey@users.noreply.github.com>
2024-12-18 13:03:17 +0530

    Time: 52 ms (7.1%), Space: 16.5 MB (99.71%) - LeetHub

---
+++ 0799-minimum-distance-between-bst-nodes/0799-minimum-distance-between-bst-nodes.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDiffInBST(self, root: Optional[TreeNode]) -> int:
+        def trav(root, final):
+            # If the root is None, return None
+            if not root:
+                return None
+
+            # Append the value of the root to the final list
+            final.append(root.val)
+
+            # Recursively traverse the left and right subtrees
+            trav(root.left, final)
+            trav(root.right, final)
+
+            # Return the final list
+            return final
+
+        # Recursively traverse the tree and get all the values
+        values = trav(root, [])
+
+        # Sort the values
+        values.sort()
+
+        # Find the minimum difference between any two consecutive values
+        min_diff = 100000
+        for i in range(1, len(values)):
+            if values[i] - values[i - 1] < min_diff:
+                min_diff = values[i] - values[i - 1]
+
+        return min_diff
