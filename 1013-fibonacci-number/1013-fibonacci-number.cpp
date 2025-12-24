c2d6f80372bf505b956f2085488a29c61117735a
Anushk Pandey <98946689+pxndey@users.noreply.github.com>
2025-12-24 12:45:46 -0500

    1013-FIBONACCI-NUMBER Stats: Time: 0 ms (100%), Space: 7.7 MB (87.1%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     int fib(int n) {
-        vector<int> v;
-        v.push_back(0);
-        v.push_back(1);
-        int temp;
+        int last = 1;
+        int secondLast = 0;
+        if(n==0) return secondLast;
+        if(n==1) return last;
+        int fib = 0;
         for(int i=2;i<=n;i++){
-            temp = v.at(i-1)+v.at(i-2);
-            v.push_back(temp);
-        } 
-        return v.at(n);
+            fib = last + secondLast;
+            secondLast = last;
+            last = fib;
+        }
+        return fib;
     }
+
 };
